f5179b2bb3d0c8cc35e6062bd2469d8d132e8366
Theproccy <61127796+Theproccy@users.noreply.github.com>
2021-10-17 23:28:20 +0100

    Formatting update to be the same style as the main program

--- Knobs.py
+++ Knobs.py
@@ -1,23 +1,27 @@
+import rich
+
+
 def knobs():
     up = ["001011111101", "101010011011"]
     down = ["011001111101", "101010010001"]
     left = ["000010100111", "000010000110"]
     right = ["101111111010", "101100111010"]
-
+    answer = ""
     sequence_input = input("Please enter the sequence of LEDs(E.G. 001011111101)(1 meaning light is on): ")
     exit_knobs = True
     if sequence_input == up[0] or sequence_input == up[1]:
-        print("Up")
+        answer = "Up"
     elif sequence_input == down[0] or sequence_input == down[1]:
-        print("Down")
+        answer = "Down"
     elif sequence_input == left[0] or sequence_input == left[1]:
-        print("Left")
+        answer = "Left"
     elif sequence_input == right[0] or sequence_input == right[1]:
-        print("Right")
+        answer = "Right"
     elif sequence_input == "":
         exit_knobs = True
     else:
-        print("Data was entered wrong try again.")
+        answer = "Data was entered wrong try again."
+    rich.print("[bold green]" + answer + "[/bold green]")
     return exit_knobs
 
 
